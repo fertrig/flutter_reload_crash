e9293ca3fb63519baecc2b69f5f7f6e4029778c8
Fernando Trigoso <ftrigoso@dropsource.com>
2023-02-14 16:49:41 -0500

    mockito crash reproducible, removed import to mockito from cat.dart, not needed to reproduce issue

--- cat/lib/cat.dart
+++ cat/lib/cat.dart
@@ -1,10 +1,9 @@
 import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 
 @GenerateNiceMocks([MockSpec<Cat>()])
 import 'cat.mocks.dart';
 
 // Real class
 class Cat {
-  String sound() => "ppp";
+  String sound() => "www";
 }
