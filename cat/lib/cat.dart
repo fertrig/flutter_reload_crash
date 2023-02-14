f4a959b5646c8582204b8a6a9a4fb24dc14f9959
Fernando Trigoso <ftrigoso@dropsource.com>
2023-02-14 17:47:14 -0500

    crashes without generated mock, crashes using simple imports without package prefix, crashes using cyclical imports and extending Mock

--- cat/lib/cat.dart
+++ cat/lib/cat.dart
@@ -1,9 +1,6 @@
-import 'package:mockito/annotations.dart';
-
-@GenerateNiceMocks([MockSpec<Cat>()])
-import 'cat.mocks.dart';
+import 'cat_extra.dart';
 
 // Real class
 class Cat {
-  String sound() => "www";
+  String sound() => "eee";
 }
--- cat/lib/main.dart
+++ cat/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:cat/cat.dart';
+import 'cat.dart';
 
 void main() {
   runApp(const MyApp());
