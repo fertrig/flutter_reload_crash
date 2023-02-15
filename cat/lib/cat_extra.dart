1dbfff4d2a8dafc73d908f4b1926a6a1711f845c
Fernando Trigoso <ftrigoso@dropsource.com>
2023-02-15 11:38:15 -0500

    crashes, no code changes, comment that extending SmartFake also crashes

--- cat/lib/cat_extra.dart
+++ cat/lib/cat_extra.dart
@@ -2,3 +2,8 @@ import 'cat.dart'; // comment this out and it doesn't crash
 import 'package:mockito/mockito.dart';
 
 class CatExtra extends Mock {}
+
+/// It also crashes when extending SmartFake instead of Mock
+// class CatExtra extends SmartFake {
+//   CatExtra(super.parent, super.parentInvocation);
+// }
