a29c850ad7f35cfe81bb831ad7ba5e68436b0b04
Fernando Trigoso <ftrigoso@dropsource.com>
2023-02-15 14:20:55 -0500

    crashes without fish_extra.dart, crashes with FishFoo inside of fish.dart, simpler crash

--- fish/lib/fish.dart
+++ fish/lib/fish.dart
@@ -1,5 +1,20 @@
-import 'fish_extra.dart';
+import 'foo.dart';
 
 class Fish {
-  String sound() => "ooo";
+  String sound() => "ccc";
+}
+
+
+class FishFoo extends Foo<bool, int> {
+  @override
+  bool fromFoo(int foo) {
+    // TODO: implement fromFoo
+    throw UnimplementedError();
+  }
+
+  @override
+  int toFoo(bool object) {
+    // TODO: implement toFoo
+    throw UnimplementedError();
+  }
 }
