b990fd3caaceedce4154a16557e20fe0687da4c6
Fernando Trigoso <ftrigoso@dropsource.com>
2023-02-15 14:45:25 -0500

    crashes still, merged Fish and FishFoo into Fish, no need for class FishFoo for crash, Fish can extend Foo and it still crashes

--- fish/lib/fish.dart
+++ fish/lib/fish.dart
@@ -1,11 +1,7 @@
 import 'foo.dart';
 
-class Fish {
-  String sound() => "qqq";
-}
-
-
-class FishFoo extends Foo {
+class Fish extends Foo {
+  String sound() => "aaa";
   @override
   void bar() {
     // TODO: implement bar
